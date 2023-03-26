9a1c4a95d922df133c4150b65fec73a45ea8a235
sereggan <sergei.ni@yandex.ru>
2023-03-26 01:05:28 +0100

    first commit

---
+++ src/main/java/com/example/servicemesh/ServiceMeshApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicemesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMeshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMeshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/servicemesh/ServiceMeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicemesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMeshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
